8fc101aeaaa9ec2b2144d90a15e7694a8a62a9e0
Matthew Coronel <coronel@hartford.edu>
2023-03-07 12:10:00 -0500

    gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { watch, series } = require(`gulp`),
+    browserSync = require(`browser-sync`);
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        server: {
+            baseDir: `./`
+        }
+    });
+
+    watch(`js/*.js`).on(`change`, browserSync.reload);
+    watch(`css/*.css`).on(`change`, browserSync.reload);
+    watch(`*.html`).on(`change`, browserSync.reload);
+};
+exports.build = serve;
+exports.default = series(serve);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+const jsonData = '../json/data.json';
+
 window.onload = () => {
+       console.log(jsonData)
        alert('Test');                    
 }
 
